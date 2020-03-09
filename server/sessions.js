cb3e6d7389bb85832ec2d37ff2fed546f8807b20
Erik Hughes <erik.hughes@outlook.com>
2020-03-09 14:34:29 +0100

    feat: added limit of 10 players per session

--- server/sessions.js
+++ server/sessions.js
@@ -126,6 +126,8 @@ sessionEndpoints.put('/:code', (req, res) => {
   const { code, session } = getSession(req.params.code, false);
   if (!session) return errorMessage(res, 404, `Session ${code} does not exist`);
   if (session.stage !== 'lobby') return errorMessage(res, 404, `Session ${code} has already started`);
+  if (req.body.participant && session.getIds(true).length >= 10)
+    return errorMessage(res, 403, `Session ${code} already has the maximum of 10 participants`);
   const client = session.newClient(req.body.name, req.body.participant);
   res.send({ code, client });
 });
